dee7bf3ce95b1f56aa5e40d7394cc4837b2ed918
Ff Tt <ft@1tcy.de>
2013-10-19 16:01:09 +0200

    safe

--- hexchat/bitlbee-identifier.py
+++ hexchat/bitlbee-identifier.py
@@ -11,45 +11,69 @@
 import hexchat
 import re
 
-__module_name__ = "bitlbee-identify"
+__module_name__ = "BitlBee Identifier"
 __module_version__ = "0.1"
 __module_description__ = "Send identify command to BitlBee channel"
 __author__ = "F T (ft2011)"
 
-CMD_PREFIX = "bbi"
-CONST_CMDS = ["help", "set", "show", "clear"]
-CONST_PREFS = ["server", "chan", "pass", "nick",  "debug"]
-DEF_PREFS = ["irc.c.y", "#some", "pass123", "yaname",  False]
+CMDS = 	{ 
+			"bbi" : ["help", "set", "show", "clear"] 
+		}
+PREFS = {
+			"server" : "(.*)\.efnet\.[net|com]",
+			"chan" : "#chan[one|two]",
+			"pass" : "pass123",
+			"debug" : "False"
+		}
 
+HELP =	[
+			"...........",
+			"...........",
+			".../bbi help",
+			"...........",
+			"...........this",
+			"...........",
+			"...........",
+			".../bbi set <server> <channel> <password>",
+			"...........",
+			"...........set values!",
+			"...........server and channel are regex!",
+			"...........example:",
+			".........../bbi set (.*)\.efnet\.[net|com] #chan[one|two] pass123",
+			"...........",
+			"...........",
+			".../bbi show",
+			"...........",
+			"...........displays current values this",
+			"...........",
+			"...........",
+			".../bbi match <regex1> <regex2>",
+			"...........",
+			"...........test regex test",
+			"...........",
+			"..........."
+		]
+
+trigger = "bbi"
+
+
+### basic getter / setter
 def showHelp():
-	hexchat.prnt("---------------")
-	hexchat.prnt("/{} {}".format(CMD_PREFIX, CONST_CMDS[0]))
-	hexchat.prnt("\t\t\t - show this")
-
-	hexchat.prnt("/{} {} <server> <channel> <password> [<nick>] [<debug>]".format(CMD_PREFIX, CONST_CMDS[0]))
-	hexchat.prnt("\t\t\t - set values!")
-	hexchat.prnt("\t\t\t - server and channel are regex!")
-	hexchat.prnt("\t\t\t - example: /{} {} (.*)\.efnet\.[net|com] #chan[one|two] mypassword123".format(CMD_PREFIX, CONST_CMDS[1]))
-
-	hexchat.prnt("/{} {}".format(CMD_PREFIX, CONST_CMDS[2]))
-	hexchat.prnt("\t\t\t - show values!")
-	hexchat.prnt("---------------")
+	for line in HELP:
+		hexchat.prnt(line)
 
 def clearVals():
-	for k in range(len(CONST_PREFS)):
-		hexchat.set_pluginpref(CONST_PREFS[k], "")
+	for k in PREFS.keys():
+		hexchat.set_pluginpref(PREFS[k], "")
 
-def saveVals(vals, nick):
-	hexchat.prnt("------> new values: '{}'".format(str(vals)))
-	for k in range(len(vals)):
-		hexchat.set_pluginpref(CONST_PREFS[k], vals[k])
-
-	hexchat.set_pluginpref("nick", nick)
+def saveVals(vals):
+	for i,k in enumerate(PREFS.keys()):
+		hexchat.prnt("{} {}".format(str(i), str(k)))
+		hexchat.set_pluginpref(str(PREFS[k]), vals[i])
 
 def showVals():
-	hexchat.prnt("------------")
-	for k in range(len(CONST_PREFS)):
-		hexchat.prnt(hexchat.get_pluginpref(CONST_PREFS[k]))
+	for k in PREFS.keys():
+		hexchat.prnt(hexchat.get_pluginpref(k))
 
 def gV(key):
 	return hexchat.get_pluginpref(key)
@@ -81,32 +105,37 @@ def onJoin_cb(word, word_eol, userdata):
 	hexchat.command("msg {} identify {}".format(chan, gV("pass")))
 
 def onCommand_cb(word, word_eol, userdata):
-	if word[1] == CONST_CMDS[0]: # show help
+
+	hexchat.prnt(word_eol[0])
+
+	if word[1] == CMDS[trigger][0]: # show help
 		showHelp()
 		return hexchat.EAT_ALL
 	
-	elif word[1] == CONST_CMDS[1]: # save values
-		nick = hexchat.get_info("nick")
-		saveVals(word[2:], nick)
+	elif word[1] == CMDS[trigger][1]: # save values
+		tmp = word[2:]
+		tmp.append("False")
+		saveVals(tmp)
 		return hexchat.EAT_ALL
 
-	elif word[1] == CONST_CMDS[2]:  # show values
+	elif word[1] == CMDS[trigger][2]:  # show values
 		showVals()
 		return hexchat.EAT_ALL
 
-	elif word[1] == CONST_CMDS[3]:  # clear values
+	elif word[1] == CMDS[trigger][3]:  # clear values
 		clearVals()
 		return hexchat.EAT_ALL
 
+
 	hexchat.prnt("unknown command: '/{}'".format(word_eol[0]))
-	hexchat.prnt("check out: /{} {}".format(CMD_PREFIX, CONST_CMDS[0]))
+	hexchat.prnt("check out: /{} {}".format(trigger, CMDS[trigger][0]))
 	return hexchat.EAT_ALL
 
 def unload_cb(userdata):
 	hexchat.prnt("BitlBee-identify unloaded!")
 
 ### setting up hook
-hexchat.hook_command(CMD_PREFIX, onCommand_cb)
+hexchat.hook_command(trigger, onCommand_cb)
 hexchat.hook_server("JOIN", onJoin_cb)
 hexchat.hook_unload(unload_cb)
 
